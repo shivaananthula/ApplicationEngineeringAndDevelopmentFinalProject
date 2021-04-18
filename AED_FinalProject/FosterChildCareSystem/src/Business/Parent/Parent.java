131c8d4d737f2fb0e0541c51b31d5ddb947abeb2
ANANTHULA_SHIVA_SHANKAR_001549845 <ananthula.sh@northeastern.edu>
2021-04-17 21:10:30 -0400

    17April commits with ParentDirectory and UserAccountDirectory

---
+++ AED_FinalProject/FosterChildCareSystem/src/Business/Parent/Parent.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Parent;
+
+/**
+ *
+ * @author shiva
+ */
+public class Parent {
+    
+    public String Name;
+    
+    public int Age;
+    
+    public String Address;
+    
+    public int ParentId;
+    
+    public String PhoneNumber;
+    
+    public String Email;
+
+    public Parent(String Name, int Age, String Address, int ParentId, String PhoneNumber, String Email) {
+        this.Name = Name;
+        this.Age = Age;
+        this.Address = Address;
+        this.ParentId = ParentId;
+        this.PhoneNumber = PhoneNumber;
+        this.Email = Email;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int Age) {
+        this.Age = Age;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String Address) {
+        this.Address = Address;
+    }
+
+    public int getParentId() {
+        return ParentId;
+    }
+
+    public void setParentId(int ParentId) {
+        this.ParentId = ParentId;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String PhoneNumber) {
+        this.PhoneNumber = PhoneNumber;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+    
+    
+}
