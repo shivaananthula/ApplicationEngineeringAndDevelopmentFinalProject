2413f7a2c46c3d7110183d1bfd23a6c68f6d7947
Koushik Kumar Karre <koushik@Koushiks-Air.cable.rcn.com>
2021-04-19 18:23:29 -0400

    Added rental and training panels

--- AED_FinalProject/FosterChildCareSystem/src/userinterface/SystemAdminWorkArea/TrainingEntAdminWorkAreaPanel.java
+++ AED_FinalProject/FosterChildCareSystem/src/userinterface/SystemAdminWorkArea/TrainingEntAdminWorkAreaPanel.java
@@ -27,7 +27,7 @@ public class TrainingEntAdminWorkAreaPanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jSplitPane1 = new javax.swing.JSplitPane();
+        TrainingSplit = new javax.swing.JSplitPane();
         jPanel1 = new javax.swing.JPanel();
         btnManageReq = new javax.swing.JButton();
         btnManageOrg = new javax.swing.JButton();
@@ -35,9 +35,14 @@ public class TrainingEntAdminWorkAreaPanel extends javax.swing.JPanel {
         btnManageUserAcc = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
 
-        jSplitPane1.setDividerLocation(200);
+        TrainingSplit.setDividerLocation(200);
 
         btnManageReq.setText("Manage Requests");
+        btnManageReq.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnManageReqActionPerformed(evt);
+            }
+        });
 
         btnManageOrg.setText("Manage Organization");
         btnManageOrg.addActionListener(new java.awt.event.ActionListener() {
@@ -47,8 +52,18 @@ public class TrainingEntAdminWorkAreaPanel extends javax.swing.JPanel {
         });
 
         btnManageEmp.setText("Manage Employee");
+        btnManageEmp.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnManageEmpActionPerformed(evt);
+            }
+        });
 
         btnManageUserAcc.setText("Manage User Account");
+        btnManageUserAcc.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnManageUserAccActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -77,7 +92,7 @@ public class TrainingEntAdminWorkAreaPanel extends javax.swing.JPanel {
                 .addContainerGap(220, Short.MAX_VALUE))
         );
 
-        jSplitPane1.setLeftComponent(jPanel1);
+        TrainingSplit.setLeftComponent(jPanel1);
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
@@ -90,32 +105,52 @@ public class TrainingEntAdminWorkAreaPanel extends javax.swing.JPanel {
             .addGap(0, 437, Short.MAX_VALUE)
         );
 
-        jSplitPane1.setRightComponent(jPanel2);
+        TrainingSplit.setRightComponent(jPanel2);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jSplitPane1)
+            .addComponent(TrainingSplit)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jSplitPane1)
+            .addComponent(TrainingSplit)
         );
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnManageOrgActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnManageOrgActionPerformed
         // TODO add your handling code here:
+        TrainingEntManageOrgJPanel trainingorg=new TrainingEntManageOrgJPanel();
+        TrainingSplit.setRightComponent(trainingorg);
     }//GEN-LAST:event_btnManageOrgActionPerformed
+
+    private void btnManageEmpActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnManageEmpActionPerformed
+        // TODO add your handling code here:
+        TrainingEntManageEmpJPanel trainingemp=new TrainingEntManageEmpJPanel();
+        TrainingSplit.setRightComponent(trainingemp);
+    }//GEN-LAST:event_btnManageEmpActionPerformed
+
+    private void btnManageUserAccActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnManageUserAccActionPerformed
+        // TODO add your handling code here:
+        TrainingEntManageUserAccJPanel traininguser=new TrainingEntManageUserAccJPanel();
+        TrainingSplit.setRightComponent(traininguser);
+    }//GEN-LAST:event_btnManageUserAccActionPerformed
+
+    private void btnManageReqActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnManageReqActionPerformed
+        // TODO add your handling code here:
+        TrainingEntManageReqJPanel trainingReq=new TrainingEntManageReqJPanel();
+        TrainingSplit.setRightComponent(trainingReq);
+    }//GEN-LAST:event_btnManageReqActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JSplitPane TrainingSplit;
     private javax.swing.JButton btnManageEmp;
     private javax.swing.JButton btnManageOrg;
     private javax.swing.JButton btnManageReq;
     private javax.swing.JButton btnManageUserAcc;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
-    private javax.swing.JSplitPane jSplitPane1;
     // End of variables declaration//GEN-END:variables
 }
